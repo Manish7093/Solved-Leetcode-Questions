cf17bd6256429c620fba34f6937fd9f27ecc13a5
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-01-27 21:21:05 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (72.76%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int count{0};
-        for(int i=31;i>=0;i--){
-           int mask=1<<i;
+        for(int i=0;i<32;i++){
+            int mask=1<<i;
             if((mask&x)!=(mask&y))
                 count++;
         }
