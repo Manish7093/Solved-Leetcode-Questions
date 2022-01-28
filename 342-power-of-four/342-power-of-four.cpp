8ae8feb07d3ab49973aa2fcb453fd7e4c404fab1
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-01-28 21:26:32 +0530

    Time: 3 ms (43.02%), Space: 6 MB (24.94%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -8,12 +8,23 @@ public:
             n=n/4;
         }
         return n==1;*/
-        if(n<=0)
+   //TC-O(1)
+        
+        /*   if(n<=0)
             return false;
         int mask=n&(n-1);
         if(mask==0 && (n-1)%3==0)
             return true;
         else
+            return false;*/
+     //TC-O(1)
+        if(n<=0)
             return false;
+        int mask=n&(n-1);
+        if(mask==0 && (n&0x55555555)==n)
+            return true;
+        else
+            return false;
+        
     }
 };
