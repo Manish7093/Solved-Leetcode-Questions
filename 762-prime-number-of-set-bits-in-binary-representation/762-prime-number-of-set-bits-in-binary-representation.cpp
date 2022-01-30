00da94790b27697e03f71ad2d16496c719ca9661
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-01-31 01:53:02 +0530

    Time: 6 ms (81.63%), Space: 6 MB (47.23%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*ass Solution {
 public:
     int count(int n){
         int count{0};
@@ -33,6 +33,31 @@ public:
                sum=sum+1;
            left=left+1;
        }
+        return sum;
+    }
+};*/
+
+class Solution {
+public:
+    bool isprime(int n){
+        if(n==2||n==3||n==5||n==7||n==11||n==13||n==17||n==19||n==23||n==29||n==31)
+            return true;
+        return false;
+    }
+    int count(int n){
+        int count{0};
+        while(n!=0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+    int countPrimeSetBits(int left, int right) {
+     int sum{0};
+        while(left<=right){
+         if(isprime(count(left++)))
+             sum=sum+1;
+     }
         return sum;
     }
 };
