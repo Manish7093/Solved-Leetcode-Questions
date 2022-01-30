9716ca338d790d50df7148aebe00835c6b17d3eb
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-01-30 19:27:26 +0530

    Time: 20 ms (98.54%), Space: 21.5 MB (64.46%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n=nums.size();
+  // Tc-O(nlogn) sc-O(1)
+        /*   int n=nums.size();
         vector<int>res;
         sort(nums.begin(),nums.end());
         int sum{0};
@@ -21,6 +22,19 @@ public:
             res.push_back(k+sum);
         else
             res.push_back(k+sum);
+        return res;*/
+        vector<int>res;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+           if(nums[abs(nums[i])-1]<0)
+               res.push_back(abs(nums[i]));
+            else
+                nums[abs(nums[i])-1]*=-1;
+        }
+        for(int i=0;i<n;i++)
+            if(nums[i]>0)
+                res.push_back(i+1);
         return res;
+        
     }
 };
