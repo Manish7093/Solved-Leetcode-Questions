a68c1f324957d057684955e43eff7a36093dd98e
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-01-30 22:21:23 +0530

    Time: 5 ms (8.55%), Space: 6 MB (22.99%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int d=n&1;
+    //TC-O(32)
+        /*   int d=n&1;
         while((n&1)==d){
             d=1-d;
             n=n>>1;
         }
-        return n==0;
+        return n==0;*/
+        //TC-O(1)    Can give overflow for very large nos but it is true
+    /*    n = n ^ (n>>1);
+        return (n & n+1) == 0;*/
+        //TC-O(1)
+         return (n & (n >> 1)) == 0 && (n | (n >> 2)) == n;
+        
+      
     }
 };
