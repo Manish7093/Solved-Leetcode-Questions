bf4ffd51476ac020322e49c0bec352294b8d6e3f
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-02-01 00:09:56 +0530

    Time: 56 ms (19.12%), Space: 24.9 MB (66.16%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-       int n=encoded.size();
+      //TC-O(n) sc-O(1)
+    
+        int n=encoded.size();
         vector<int>res(n+1);
         res[0]=first;
         int i{0};
-        for( i=1;i<n;i++){
+        for( i=1;i<=n;i++){
             res[i]=encoded[i-1]^res[i-1];
         }
-        res[n]=res[n-1]^encoded[n-1];
+    //    res[n]=res[n-1]^encoded[n-1];
         return res;
+        
     }
 };
