bc9b833b7e8259f288fd74c84d9c8bad83adfee0
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-01-31 21:19:40 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.89%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,19 +1,44 @@
+/*
 class Solution {
 public:
     int xorOperation(int n, int start) {
-       /* vector<int>nums(n);
+        vector<int>nums(n);
         int xr{0};
         for(int i=0;i<n;i++){
             nums[i]=start+2*i;
             xr=xr^nums[i];
         }
         return xr;*/
-        int xr{0};
+      //TC-O(n)
+        /*  int xr{0};
         for(int i=0;i<n;i++){
             int k=0;
             k=start+2*i;
             xr=xr^k;
         }
         return xr;
+    
+    }
+};*/
+class Solution {
+public:
+    int xorOperationB(int n,int start){
+        if(n%2==0)
+            return (n/2)&1;
+        else
+            return ((n/2)&1)^(start+n-1);
+    }
+    int xorOperationA(int n,int start){
+        if(start&1)
+            return (start-1)^xorOperationB(n+1,start-1);
+        else
+            return xorOperationB(n,start);
+    }
+    int xorOperation(int n, int start) {
+    int res=2*(xorOperationA(n,start/2));
+        if(n&start&1)
+            res++;
+        return res;
+    
     }
 };
