be43a01c2a7b5aa96cf0ba98e2b11cf0ca435315
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-02-06 23:48:19 +0530

    Time: 12 ms (9.79%), Space: 11.2 MB (29.40%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int small(vector<int>nums){
         int n=nums.size();
@@ -38,6 +38,34 @@ public:
            return binarys(nums,0,sm-1,target);
         else
            return binarys(nums,sm,n-1,target);
+        
+    }
+};*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int n=nums.size();
+        int start=0,end=n-1;
+        
+        while(start<=end){
+             int mid=start+(end-start)/2;
+            if(target==nums[mid])
+                return mid;
+            else if(nums[mid]>=nums[start]){
+                if(target>=nums[start] && target<=nums[mid]){
+                    end=mid-1;
+                }
+                else
+                    start=mid+1;
+            }
+            else{
+                if(target<=nums[end] && target>=nums[mid])
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }
+        }
+        return -1;
         
     }
 };
