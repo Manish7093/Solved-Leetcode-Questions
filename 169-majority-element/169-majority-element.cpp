8073315f897621098d10d773900454dc25d4bc0f
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-02-22 01:33:09 +0530

    Time: 20 ms (78.20%), Space: 19.6 MB (52.43%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
+     //TC-O(n) SC-O(n)
+        /*  int n=nums.size();
         unordered_map<int,int>mpp;
         for(int i=0;i<n;i++)
             mpp[nums[i]]+=1;
@@ -9,6 +10,20 @@ public:
             if(mpp[nums[i]]>(n/2))
                 return nums[i];
         }
-        return 0;
+        return 0;*/
+    //TC-O(n) SC-O(1)
+        int n=nums.size();
+        int count=1,major=nums[0];
+        for(int i=1;i<n;i++){
+            if(count==0){
+                count++;
+                major=nums[i];
+            }
+            else if(major==nums[i])
+                count++;
+            else
+                count--;
+        }
+     return major;   
     }
 };
