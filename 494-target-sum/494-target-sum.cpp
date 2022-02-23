3c1ed7d3309a9e4122fbaa607e977ee6ac5b1239
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-02-24 00:13:49 +0530

    Time: 8 ms (91.90%), Space: 9.2 MB (60.58%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countzero(vector<int>&nums){
+        int count{0};
+        int n=nums.size();
+        for(int j=0;j<n;j++)
+            if(nums[j]==0)
+                count++;
+        return count;
+    }
+    int countSubsetSum(vector<int>nums,int n,int sum){
+       int mod=1e9+7;
+        int dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0)
+                    dp[i][j]=0;
+                if(j==0)
+                    dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(nums[i-1]>j ||nums[i-1]==0)
+                    dp[i][j]=(dp[i-1][j])%mod;
+                else
+                    dp[i][j]=(dp[i-1][j-nums[i-1]]+dp[i-1][j])%mod;
+            }
+        }
+        
+        return (dp[n][sum])*pow(2,countzero(nums));
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int tar=abs(target); 
+        int n=nums.size();
+        int range{0};
+        for(int i=0;i<n;i++)
+            range+=nums[i];
+        int sum=(tar+range)/2;
+        if((tar+range)%2==1)
+            return 0;
+        return countSubsetSum(nums,n,sum);
+    }
+};
