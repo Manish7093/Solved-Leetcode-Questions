6b928b748e29fb297682dc6eeb85eb6f98fddc09
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-03-02 01:06:00 +0530

    Time: 6 ms (23.26%), Space: 6.5 MB (24.64%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int lcs(string s,string t){
            int m=s.length(),n=t.length();
@@ -27,5 +27,28 @@ public:
         if(k==j)
             return true;
         return false;
+    }
+};*/
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+          int m=s.length(),n=t.length();
+        bool dp[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            dp[i][0]=false;
+        }
+        for(int j=0;j<n+1;j++)
+            dp[0][j]=true;
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s[i-1]==t[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+      
     }
 };
