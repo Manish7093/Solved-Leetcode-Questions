689906022bbe957de8e71ac0868636982e4f272b
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-03-08 00:45:16 +0530

    Time: 53 ms (49.92%), Space: 15.3 MB (13.03%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int minimumDeleteSum(string s1, string s2) {
-        int m = s1.size(), n = s2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        for (int j = 1; j <= n; j++)
-            dp[0][j] = dp[0][j-1]+s2[j-1];
-        for (int j = 1; j <= m; j++)
-            dp[j][0] = dp[j-1][0]+s1[j-1];
-        
-        
-        for (int i = 1; i <= m; i++) {
-           // dp[i][0] = dp[i-1][0]+s1[i-1];
-            for (int j = 1; j <= n; j++) {
-                if (s1[i-1] == s2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
-                else 
-                    dp[i][j] = min(dp[i-1][j]+s1[i-1], dp[i][j-1]+s2[j-1]);
+    int minDistance(string word1, string word2) {
+        int n=word1.length(),m=word2.length();
+        int dp[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0||j==0)
+                    dp[i][j]=0;
             }
         }
-        return dp[m][n];
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int lcs=dp[n][m];
+        
+        int d=(n-lcs)+(m-lcs);
+        return d;
     }
 };
