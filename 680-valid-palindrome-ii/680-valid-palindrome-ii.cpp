3ca22e7ce5221749a898ee11bc37355ca03da7ad
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-03-11 01:40:05 +0530

    Time: 73 ms (47.58%), Space: 24 MB (30.31%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool ispalindrome(string s,int l,int r ){
+         while(l<r){
+            if(s[l]==s[r]){
+                l++;r--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int n=s.length();
+        int l=0,r=n-1;
+        while(l<r){
+            if(s[l]==s[r]){
+                l++;r--;
+            }
+            else
+                return ispalindrome(s,l+1,r)||ispalindrome(s,l,r-1);
+        }
+        return true; 
+    }
+};
