b1abac2a3c71d85ce74837057820af59f0ccd4a6
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-03-11 01:05:20 +0530

    Time: 8 ms (58.19%), Space: 7.4 MB (76.98%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length()==0)
+            return true;
+        int n=s.length();
+        int l=0,r=n-1;
+        while(l<=r){
+            if(!isalnum(s[l]) ) l++;
+            else if(!isalnum(s[r])) r--;
+            else if(tolower(s[l])!=tolower(s[r]))
+                return false;
+            else{
+                l++;r--;
+            }
+        }
+        return true;
+    }
+};
