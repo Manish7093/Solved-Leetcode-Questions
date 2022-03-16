9f0512abd22753cd6971c11f9cf291bac7bc14b7
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-03-16 13:44:52 +0530

    Time: 22 ms (12.18%), Space: 15.2 MB (96.74%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int index=0;
+      /*  int index=0;
         stack<int>stk;
         for(int val:pushed){
             stk.push(val);
@@ -13,6 +13,18 @@ public:
         if(stk.empty())
             return true;
         
+        return false;*/
+        int index=0;
+        int count{0};
+        for(int val:pushed){
+        pushed[count]=val;
+            while(count>=0 && popped[index]==pushed[count]){
+                count--;index++;
+            }
+            count++;
+        }
+        if(count==0)
+            return true;
         return false;
     }
 };
