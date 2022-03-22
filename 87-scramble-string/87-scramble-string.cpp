13ba19b08171db00ecf6d39b1b2f9910612d578f
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-03-23 00:31:46 +0530

    Time: 196 ms (45.11%), Space: 37.2 MB (25.54%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,49 +1,14 @@
 class Solution {
 public:
     unordered_map<string,bool>mp;
-   /* bool solve(string x,string y){
-        string key=x+" "+y;
-        if(mpp.find(key)!=mpp.end())
-            return mpp[key];
-        if(x.compare(y)==0){
-            mpp[key]=true;
-            return true;
-        }
-        if(x.length()<=1){
-            mpp[key]=false;
-            return false;
-        }
-        
-        int n=x.length();
-        bool flag=false;
-        
-        for(int i=1;i<=n-1;i++){
-            if((solve(x.substr(0,i),y.substr(n-i,i)) && solve(x.substr(i),y.substr(0,n-i))) ||
-              (solve(x.substr(0,i),y.substr(0,i)) && solve(x.substr(i),y.substr(i))))
-                flag=true;
-                break;
-        }
-        return mpp[key]=flag;
-        
-    }*/
     bool isScramble(string s1, string s2) {
-      /*  vector<int>s1cnt(26);
-        vector<int>s2cnt(26);
-        for(int i=0;i<s1.size();i++){
-            s1cnt[s1[i]-'a']++;
-            s2cnt[s2[i]-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            if(s1cnt[i]!=s2cnt[i])
-                return false;
-        }
-        return solve(s1,s2);*/
+     
              if(s1.compare(s2)==0)
             return true;
         
-        string key=s1;
-        key.append(" ");
-        key.append(s2);
+        string key=s1+" "+s2;
+        /*key.append(" ");
+        key.append(s2);*/
         
         if(mp.find(key)!=mp.end())
             return mp[key];
