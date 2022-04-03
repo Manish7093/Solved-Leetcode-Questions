fcbf56e496c48cccc2438d42f9232cab3d9ac33f
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-04-03 16:31:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.03%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    bool isSumEqual(string firstWord, string secondWord, string targetWord) {
-        int res1{0};
-        int res2{0};
-        int res3{0};
-        for(int i=0;i<firstWord.size();i++)
-            res1=res1*10+(firstWord[i]-'a');    
-        for(int i=0;i<secondWord.size();i++)
-            res2=res2*10+(secondWord[i]-'a');
-        for(int i=0;i<targetWord.size();i++)
-            res3=res3*10+(targetWord[i]-'a');
-        if((res1)+(res2)==(res3))
-            return true;
-        return false;
+    int equalSubstring(string s, string t, int maxCost) {
+        int i{0}, j{0},ans{0};
+        int cost{0};
+        
+        while(j<s.size()){
+                cost+=abs(t[j]-s[j]);  
+            while(cost>maxCost){
+                cost-=abs(t[i]-s[i]);
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
     }
 };
