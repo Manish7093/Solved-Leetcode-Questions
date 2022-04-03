36d11f4d8e591628d1e45ee03e62af47a620c5a1
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-04-03 23:49:34 +0530

    Time: 54 ms (69.58%), Space: 30.9 MB (68.59%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
@@ -3,12 +3,15 @@ public:
     int atmost(vector<int> nums,int goal){
         int i{0},j{0},ans{0},sum{0};
         while(j<nums.size()){
-            sum+=nums[j];j++;
+            sum+=nums[j];
+            j++;
             while(sum>goal &&j>i){
                 sum-=nums[i];
                 i++;
             }
-            ans+=j-i;
+        
+            ans+=j-i+1;
+        
             
         }
         return ans;
