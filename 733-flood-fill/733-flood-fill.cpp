bf9aa1e60e0dde99b1f10f4b52718bc35ab95352
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-04-06 23:01:09 +0530

    Time: 24 ms (7.79%), Space: 13.8 MB (91.68%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
   void solve(vector<vector<int>>&image,int c,int i,int j,int nc){
-        if(i<0 || i==image.size() ||j<0 ||j==image[0].size()||image[i][j]!=c)
+        if(i<0 || i>=image.size() ||j<0 ||j>=image[0].size()||image[i][j]!=c)
             return;
         image[i][j]=nc;
         solve(image,c,i+1,j,nc);
