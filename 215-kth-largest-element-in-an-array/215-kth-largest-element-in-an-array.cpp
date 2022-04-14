96a1dc1002d5e6f457c39a8f3ed6261ef4c1ed93
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-04-14 23:15:22 +0530

    Time: 167 ms (7.61%), Space: 10.1 MB (60.21%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
+    int partition(vector<int>&nums,int left,int right){
+        int pivot=nums[left],l=left+1,r=right;
+        while(l<=r){
+            if(pivot>nums[l] && pivot<nums[r]){
+                swap(nums[l++],nums[r--]);
+            }if(nums[l]>=pivot)
+                l++;
+            if(nums[r]<=pivot)
+                r--;
+        }
+        swap(nums[left],nums[r]);
+        return r;
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> minheap;
+       /* priority_queue<int,vector<int>,greater<int>> minheap;
         for(int i=0;i<nums.size();i++){
             minheap.push(nums[i]);
             if(minheap.size()>k)
                 minheap.pop();
         }
-        return minheap.top();
+        return minheap.top();*/
+        //Using Quick select
+        int left=0,right=nums.size()-1,kth;
+        while(true){
+            int idx=partition(nums,left,right);
+            if(idx==k-1){
+                kth=nums[idx];
+                break;
+            }
+            if(idx<k-1){
+                left=idx+1;
+            }else{
+                right=idx-1;
+            }
+        }
+        return kth;
     }
 };
