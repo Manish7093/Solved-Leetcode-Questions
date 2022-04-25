4f60770a9630feaf97ef913254d5908cffa1ccb5
Manish7093 <86975538+Manish7093@users.noreply.github.com>
2022-04-25 11:51:39 +0530

    Time: 94 ms (7.09%), Space: 49.5 MB (6.25%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool issafe(vector<string>temp,int i,int j){
@@ -30,5 +31,40 @@ public:
         vector<string>temp(n,string(n,'.'));
         backtrack(ans,temp,n,0);
         return ans.size();
+    }
+};
+*/
+class Solution {
+public:
+    bool issafe(vector<string>temp,int i,int j){
+        for(int x=0;x<i;x++){
+            for(int y=0;y<temp.size();y++){
+            if(temp[x][y]=='Q'){
+                if(y==j || abs(x-i)==abs(y-j))
+                    return false;
+              }
+            }
+        }
+        return true;
+    }
+    void backtrack(int & count,vector<string>temp,int n,int i){
+        if(i==n){
+            count++;
+            return;
+        }
+        for(int j=0;j<n;j++){
+            if(issafe(temp,i,j)){
+                temp[i][j]='Q';
+                backtrack(count,temp,n,i+1);
+                temp[i][j]='.';
+            }
+        }
+        return;
+    }
+    int totalNQueens(int n) {
+        int count{0};
+        vector<string>temp(n,string(n,'.'));
+        backtrack(count,temp,n,0);
+        return count;
     }
 };
